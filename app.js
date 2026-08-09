079e31e8f931f7824ffe812dad53de9844b83237
amyunyu <amyun.yu@gmail.com>
2026-08-10 02:42:44 +0800

    Refactor stop article structure in app.js

--- app.js
+++ app.js
@@ -752,9 +752,9 @@ function renderDay() {
   const timeline = day.stops.map((stop, stopIndex) => {
     const isFlex = stop.status === "flex";
     return `
-      <article class="stop ${isFlex ? "is-flex" : ""}">
-        <div class="time"><span>${stop.time}</span></div>
-        <div class="stop-card" role="button" tabindex="0" data-stop-index="${stopIndex}" aria-label="查看 ${stop.title} 詳細說明">
+  <article class="stop ${isFlex ? "is-flex" : ""}">
+    <div class="stop-card" role="button" tabindex="0" data-stop-index="${stopIndex}" aria-label="查看 ${stop.title} 詳細說明">
+      <div class="time"><span>${stop.time}</span></div>
           <div class="card-topline">
             <span>${stop.eyebrow}</span>
             <span class="status ${isFlex ? "flex" : "confirmed"}">${isFlex ? "彈性" : "已確認"}</span>
